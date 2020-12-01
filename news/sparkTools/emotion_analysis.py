d2bb3487f178f8cafd574c9b578a7c21daae2bb3
PatrickDDj <55349276+PatrickDDj@users.noreply.github.com>
2020-12-01 11:04:55 +0800

    增加了注释

--- news/sparkTools/emotion_analysis.py
+++ news/sparkTools/emotion_analysis.py
@@ -12,6 +12,7 @@ sc.setLogLevel("WARN")   # 设置日志级别
 spark = SparkSession(sc)
 
 
+# 获取情感字典
 def get_emotion_words():
     emotion_lines = open(EMOTION_WORDS_PATH, 'r').readlines()
     emotion_words = {}
@@ -21,38 +22,29 @@ def get_emotion_words():
     return emotion_words
 
 
+# 通过txt文本文件分析情感构成
 def news_emotion_analysis_by_file(news_path):
 
-    # emotion_words = get_emotion_words()
     news = open(news_path, 'r').read()
 
     emotion_proportion, news = news_emotion_analysis_by_text(news)
-    # news_words = news.split(' ')
-    # wordsRDD = sc.parallelize(news_words)
-    # res = wordsRDD.filter(lambda word: word in emotion_words.keys()).map(lambda word: emotion_words[word]).map(lambda emotion: (emotion, 1)).reduceByKey(lambda e1, e2: e1+e2).sortBy(ascending=False, numPartitions=None, keyfunc=lambda x: x[0])
-    #
-    # res = res.take(3)
-    #
-    # emotion_propertion = {}
-    #
-    # cnt = 0
-    #
-    # for i in res:
-    #     emotion_propertion[i[0]] = i[1]
-    #     cnt += i[1]
-    #
-    # for i in emotion_propertion.keys():
-    #     emotion_propertion[i] = int(emotion_propertion[i]*100/cnt)
-    #
+
     return emotion_proportion, news
 
 
+# 直接通过文本分析情感构成
 def news_emotion_analysis_by_text(news):
     emotion_words = get_emotion_words()
 
-    # news = open(news_path, 'r').read()
+    # 英文文本可以直接用空格' ' 分词
     news_words = news.split(' ')
+    # 转化为RDD格式
     wordsRDD = sc.parallelize(news_words)
+
+    # 1 - 过滤空（无效）单词
+    # 2 - 过滤不在情感字典中的单词
+    # 3 - 将单个word通过情感字典映射为positive neutral negative中的一种
+    # 4 - 统计三类情感出现次数，转化为百分比作为返回值
     res = wordsRDD.filter(lambda word: word in emotion_words.keys()).map(lambda word: emotion_words[word]).map(
         lambda emotion: (emotion, 1)).reduceByKey(lambda e1, e2: e1 + e2).sortBy(ascending=False, numPartitions=None,
                                                                                  keyfunc=lambda x: x[0])
@@ -73,6 +65,3 @@ def news_emotion_analysis_by_text(news):
     return emotion_proportion, news
 
 
-# emotion_propertion = news_emotoio_analysis("20201117/14th Five-Year Plan showcases democratic policy-making - People's Daily Online.txt")
-#
-# print(emotion_propertion)
--- news/views.py
+++ news/views.py
@@ -8,12 +8,15 @@ from django.db.models import Avg
 # Create your views here.
 
 
+# 新闻首页（列表）：可通过date和type两个属性进行筛选
 def news_list(request):
     date = request.GET.get('date')
     type = request.GET.get('type')
     news_list = News.objects.all().order_by('-add_time')
     if date != "" and date != None:
         news_list = news_list.filter(add_time=date)
+
+    # 将某种情感占比大于40%的新闻定义为改种情感类型的新闻
     if type != "" and type != None:
         if type == 'positive':
             news_list = news_list.filter(positive__gte=40).order_by('-positive')
@@ -25,11 +28,14 @@ def news_list(request):
     return render(request, 'news/news-list.html', context)
 
 
+# 导入新闻以及其情感分析结果至数据库中
 def news_import(request):
     date = request.GET.get('date')
     dict_path = 'static/news/'+str(date)
     files = os.listdir(dict_path)
 
+    # 分析当日所有新闻情感构成
+    # 导入结果到数据库中
     for file in files:
         emotion_proportion, content = news_emotion_analysis_by_file(dict_path + '/' + file)
 
@@ -44,18 +50,21 @@ def news_import(request):
     return redirect('news:news-list')
 
 
+# 下载当日新闻到本地
 def news_download(request):
+    # 调用爬虫函数将当日新闻下载到本地指定文件夹中
     crawl()
     return HttpResponse('Success!')
 
 
+# 分析近日新闻情感趋势
 def news_trend(request):
     daily_proportion_list = News.objects.values('add_time').annotate(
         positive_score = Avg('positive'),
         neutral_score = Avg('neutral'),
         negative_score = Avg('negative')
     )
-
+    # 获取数据库中所有新闻的日期,根据不同日期进行分组（group操作）
     time_list = list(daily_proportion_list.values_list('add_time', flat=True))
     time_list = [str(i).replace('&#39', '') for i in time_list]
 
@@ -65,13 +74,16 @@ def news_trend(request):
                'neutral_list': list(daily_proportion_list.values_list('neutral_score', flat=True)),
                'negative_list': list(daily_proportion_list.values_list('negative_score', flat=True))}
 
+    # 前端接受各个参数用于绘制情感构成直方图
     return render(request, 'news/news-trend.html', context)
 
 
+# 导航至实时分析情感构成界面
 def news_real_time_input(request):
     return render(request, 'news/news-real-time-input.html')
 
 
+# 获取用户输入的英文文本，调用文本分析函数获取情感构成
 def news_real_time_result(request):
     news = request.POST.get('news')
 
